7c6b76387b0a77f33b81f8bc1dff73b9faada2eb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-14 13:56:11 +0530

    Code Modifications

---
+++ ai102demos/TextAnalyticsDemos/AAI.TextAnalyticsApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddUserSecrets("fb603ff5-AzAIServices")
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .Build();
