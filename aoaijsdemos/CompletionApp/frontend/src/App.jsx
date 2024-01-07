e61113d54a6ddec5937df9b81354c68e1b30755c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-07 22:35:51 +0530

    Added NavBar

--- aoaijsdemos/CompletionApp/frontend/src/App.jsx
+++ aoaijsdemos/CompletionApp/frontend/src/App.jsx
@@ -2,15 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './screens/Home'
 import './App.css'
 import "./styles/bootstrap-custom.scss"
+import NavBar from './nav/NavBar';
 
 const App = () => {
   return (
     <>
-      <nav className="navbar fixed-top navbar-dark bg-primary">
+      {/* <nav className="navbar fixed-top navbar-dark bg-primary">
         <div className="container">
           <a className="navbar-brand" href="#">Completion App - Vite, React JS, Azure Open AI</a>
         </div>
-      </nav>
+      </nav> */}
+
+      <NavBar />
       <div className="container my-5">
         <Router>
           <Routes>
