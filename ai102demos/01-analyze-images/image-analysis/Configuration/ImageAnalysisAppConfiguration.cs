4db9818f593f29f781e035a8fc9aa00b6c3f4227
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-29 22:27:44 +0530

    Changes

---
+++ ai102demos/01-analyze-images/image-analysis/Configuration/ImageAnalysisAppConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace image_analysis.Configuration;
+
+public class ImageAnalysisAppConfiguration
+{
+    public string? AiServicesEndpoint { get; set; }
+
+    public string? AiServicesKey { get; set; }
+}
