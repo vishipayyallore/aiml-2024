40510734e894f7433880a09600f5a61bf70fbfac
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-27 20:08:30 +0530

    Changes

--- ai102demos/01-analyze-images/image-analysis/Program.cs
+++ ai102demos/01-analyze-images/image-analysis/Program.cs
@@ -23,7 +23,8 @@ header.DisplayHeader('=', "Azure AI Services - Image Analysis");
 
 // Get image
 //string imageFile = "images/street.jpg";
-string imageFile = "images/building.jpg";
+//string imageFile = "images/building.jpg";
+string imageFile = "images/man_door_1.jpg";
 if (args.Length > 0)
 {
     imageFile = args[0];
