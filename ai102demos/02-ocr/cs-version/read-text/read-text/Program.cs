4f66b2ca51de560d5f15c769399c0f6f068839a8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-28 14:35:55 +0530

    Update Program.cs

--- ai102demos/02-ocr/cs-version/read-text/read-text/Program.cs
+++ ai102demos/02-ocr/cs-version/read-text/read-text/Program.cs
@@ -1,4 +1,6 @@
-﻿using HeaderFooter.Interfaces;
+﻿using Azure;
+using Azure.AI.Vision.ImageAnalysis;
+using HeaderFooter.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,11 +15,50 @@ AzAISvcAppConfiguration appConfig = host.Services.GetRequiredService<AzAISvcAppC
 
 header.DisplayHeader('=', "Azure AI Services - OCR Read Text");
 
+try
+{
+    ImageAnalysisClient client = new(new Uri(appConfig.AiServicesEndpoint!), new AzureKeyCredential(appConfig.AiServicesKey!));
+
+    // Menu for text reading functions
+    WriteLine("\n1: Use Read API for image (Lincoln.jpg)\n2: Read handwriting (Note.jpg)\nAny other key to quit\n");
+    WriteLine("Enter a number:");
+    string? command = ReadLine();
+    string imageFile;
+
+    switch (command)
+    {
+        case "1":
+            imageFile = "images/Lincoln.jpg";
+            GetTextRead(imageFile, client);
+            break;
+        case "2":
+            imageFile = "images/Note.jpg";
+            GetTextRead(imageFile, client);
+            break;
+        default:
+            break;
+    }
+}
+catch (Exception ex)
+{
+    WriteLine($"\nError: {ex.Message}");
+}
+
 footer.DisplayFooter('-');
 
 ResetColor();
 WriteLine("\n\nPress any key ...");
 ReadKey();
+
+static void GetTextRead(string imageFile, ImageAnalysisClient client)
+{
+    WriteLine($"\nReading text from {imageFile} \n");
+
+    // Use a file stream to pass the image data to the analyze call
+    using FileStream stream = new(imageFile, FileMode.Open);
+
+    // Use Analyze image function to read text in image
+}
 
 static IHost GetHostBuilder(string[] args)
 {
