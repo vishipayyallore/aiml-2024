dd688a87bbfb3841d01e088263ac782f9dfa3430
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-11 13:00:30 +0530

    Initial Version

---
+++ ai102demos/03-face/cs-version/cv-detect-people/cv-detect-people/Configuration/AzAISvcAppConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace cv_detect_people.Configuration;
+
+public class AzAISvcAppConfiguration
+{
+    public string? AiServicesEndpoint { get; set; }
+
+    public string? AiServicesKey { get; set; }
+}
---
+++ ai102demos/03-face/cs-version/cv-detect-people/cv-detect-people/Program.cs
@@ -0,0 +1,19 @@
+﻿using cv_detect_people.Configuration;
+using cv_detect_people.Extensions;
+using HeaderFooter.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = IHostExtensions.GetHostBuilder(args);
+
+IHeader header = host.Services.GetRequiredService<IHeader>();
+IFooter footer = host.Services.GetRequiredService<IFooter>();
+AzAISvcAppConfiguration appConfig = host.Services.GetRequiredService<AzAISvcAppConfiguration>();
+
+header.DisplayHeader('=', "Azure AI Services - CV Detect People");
+
+footer.DisplayFooter('-');
+
+ResetColor();
+WriteLine("\n\nPress any key ...");
+ReadKey();
