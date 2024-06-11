9dea272b275e8433272c7f69d68193b90f5d5c53
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-11 13:48:35 +0530

    Code works

--- ai102demos/03-face/cs-version/cv-detect-people/cv-detect-people/Program.cs
+++ ai102demos/03-face/cs-version/cv-detect-people/cv-detect-people/Program.cs
@@ -8,6 +8,8 @@ using Azure;
 using Azure.AI.Vision.Common;
 using static System.Net.Mime.MediaTypeNames;
 using System.Drawing;
+using Image = System.Drawing.Image;
+using Font = System.Drawing.Font;
 
 using IHost host = IHostExtensions.GetHostBuilder(args);
 
@@ -48,13 +50,12 @@ ReadKey();
 
 static void AnalyzeImage(string imageFile, VisionServiceOptions serviceOptions)
 {
-    Console.WriteLine($"\nAnalyzing {imageFile} \n");
+    WriteLine($"\nAnalyzing {imageFile} \n");
 
     var analysisOptions = new ImageAnalysisOptions()
     {
-        // Specify features to be retrieved (PEOPLE)
+        Features = ImageAnalysisFeature.People,
         GenderNeutralCaption = true,
-
         ModelVersion = "latest"
     };
 
@@ -64,51 +65,49 @@ static void AnalyzeImage(string imageFile, VisionServiceOptions serviceOptions)
     // Using the correct class from the Azure AI Vision package
     using var analyzer = new ImageAnalyzer(serviceOptions, imageSource, analysisOptions);
 
-    //var result = analyzer.Analyze();
+    var result = analyzer.Analyze();
 
-    //if (result.Reason == ImageAnalysisResultReason.Analyzed)
-    //{
-    //    // Get people in the image
-    //    if (result.People != null)
-    //    {
-    //        Console.WriteLine($" People:");
+    if (result.Reason == ImageAnalysisResultReason.Analyzed)
+    {
+        // Get people in the image
+        if (result.People != null)
+        {
+            WriteLine($" People:");
 
-    //        // Prepare image for drawing
-    //        System.Drawing.Image image = System.Drawing.Image.FromFile(imageFile);
-    //        Graphics graphics = Graphics.FromImage(image);
-    //        Pen pen = new Pen(Color.Cyan, 3);
-    //        Font font = new Font("Arial", 16);
-    //        SolidBrush brush = new SolidBrush(Color.WhiteSmoke);
+            // Prepare image for drawing
+            Image image = Image.FromFile(imageFile);
+            Graphics graphics = Graphics.FromImage(image);
+            Pen pen = new(Color.Cyan, 3);
+            Font font = new("Arial", 16);
+            SolidBrush brush = new(Color.WhiteSmoke);
 
-    //        foreach (var person in result.People)
-    //        {
-    //            // Draw object bounding box if confidence > 50%
-    //            if (person.Confidence > 0.5)
-    //            {
-    //                // Draw object bounding box
-    //                var r = person.BoundingBox;
-    //                Rectangle rect = new Rectangle(r.X, r.Y, r.Width, r.Height);
-    //                graphics.DrawRectangle(pen, rect);
+            foreach (var person in result.People)
+            {
+                // Draw object bounding box if confidence > 50%
+                if (person.Confidence > 0.5)
+                {
+                    // Draw object bounding box
+                    var r = person.BoundingBox;
+                    Rectangle rect = new(r.X, r.Y, r.Width, r.Height);
+                    graphics.DrawRectangle(pen, rect);
 
-    //                // Return the confidence of the person detected
-    //                Console.WriteLine($"   Bounding box {person.BoundingBox}, Confidence {person.Confidence:0.0000}");
-    //            }
-    //        }
+                    // Return the confidence of the person detected
+                    WriteLine($"   Bounding box {person.BoundingBox}, Confidence {person.Confidence:0.0000}");
+                }
+            }
 
-    //        // Save annotated image
-    //        String output_file = "detected_people.jpg";
-    //        image.Save(output_file);
-    //        Console.WriteLine("  Results saved in " + output_file + "\n");
-    //    }
-    //}
-    //else
-    //{
-    //    var errorDetails = ImageAnalysisErrorDetails.FromResult(result);
-    //    Console.WriteLine(" Analysis failed.");
-    //    Console.WriteLine($"   Error reason : {errorDetails.Reason}");
-    //    Console.WriteLine($"   Error code : {errorDetails.ErrorCode}");
-    //    Console.WriteLine($"   Error message: {errorDetails.Message}\n");
-    //}
-
-
+            // Save annotated image
+            String output_file = "./images/detected_people.jpg";
+            image.Save(output_file);
+            WriteLine("  Results saved in " + output_file + "\n");
+        }
+    }
+    else
+    {
+        var errorDetails = ImageAnalysisErrorDetails.FromResult(result);
+        Console.WriteLine(" Analysis failed.");
+        Console.WriteLine($"   Error reason : {errorDetails.Reason}");
+        Console.WriteLine($"   Error code : {errorDetails.ErrorCode}");
+        Console.WriteLine($"   Error message: {errorDetails.Message}\n");
+    }
 }
