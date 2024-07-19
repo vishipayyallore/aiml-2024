1f1b7696b24fb19a6ecfba41c4b5d6fd11a2617b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-20 02:29:13 +0530

    Swamy/19jul work (#23)
    * Update read-text.csproj
    
    * Initial Version
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Program
    
    * Working Sample
    
    * Update Program.cs

---
+++ ai102demos/01-aoai/cs-version/CompletionsDemo/FirstSample/Extensions/AppExtensions.cs
@@ -0,0 +1,17 @@
+﻿using HeaderFooter;
+using HeaderFooter.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FirstSample.Extensions;
+
+public static class AppExtensions
+{
+    public static IServiceCollection ConfigureServices(this IServiceCollection services)
+    {
+        services.AddScoped<IHeader, Header>();
+
+        services.AddScoped<IFooter, Footer>();
+
+        return services;
+    }
+}
---
+++ ai102demos/01-aoai/cs-version/CompletionsDemo/FirstSample/Extensions/IHostExtensions.cs
@@ -0,0 +1,30 @@
+﻿using FirstSample.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FirstSample.Extensions;
+
+public static class IHostExtensions
+{
+    public static IHost GetHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((_, services) =>
+                {
+                    IConfiguration configuration = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddUserSecrets("fb603ff5-AzAIServices")
+                        .Build();
+
+                    AzAISvcAppConfiguration appConfig = new();
+                    configuration.GetSection("AzAISvcAppConfiguration").Bind(appConfig);
+
+                    services.AddSingleton(appConfig);
+
+                    services.ConfigureServices();
+                })
+                .Build();
+    }
+}
