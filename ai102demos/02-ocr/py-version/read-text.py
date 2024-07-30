c8451d13a30b7610d3a4be51ba7cabf54f723519
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-30 16:32:13 +0530

    Program working

--- ai102demos/02-ocr/py-version/read-text.py
+++ ai102demos/02-ocr/py-version/read-text.py
@@ -65,20 +65,30 @@ def GetTextRead(image_file):
         color = 'cyan'
 
         for line in result.read.blocks[0].lines:
-             # Return the text detected in the image
-            print(f"  {line.text}")    
-                
+            # Return the text detected in the image
+            print(f"  {line.text}")
+
             drawLinePolygon = True
-                
+
             r = line.bounding_polygon
-            bounding_polygon = ((r[0].x, r[0].y),(r[1].x, r[1].y),(r[2].x, r[2].y),(r[3].x, r[3].y))
-                
+            bounding_polygon = (
+                (r[0].x, r[0].y), (r[1].x, r[1].y), (r[2].x, r[2].y), (r[3].x, r[3].y))
+
             # Return the position bounding box around each line
-                
-                
+            print("   Bounding Polygon: {}".format(bounding_polygon))
+
             # Return each word detected in the image and the position bounding box around each word with the confidence level of each word
-                
-                
+            for word in line.words:
+                r = word.bounding_polygon
+                bounding_polygon = (
+                    (r[0].x, r[0].y), (r[1].x, r[1].y), (r[2].x, r[2].y), (r[3].x, r[3].y))
+                print(f"    Word: '{word.text}', Bounding Polygon: {
+                      bounding_polygon}, Confidence: {word.confidence:.4f}")
+
+                # Draw word bounding polygon
+                drawLinePolygon = False
+                draw.polygon(bounding_polygon, outline=color, width=3)
+
             # Draw line bounding polygon
             if drawLinePolygon:
                 draw.polygon(bounding_polygon, outline=color, width=3)
