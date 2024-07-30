ac324e445c3dcf962f8834dcb5537ea514d09327
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-30 14:09:33 +0530

    Changes

--- ai102demos/01-analyze-images/py-version/image-analysis.py
+++ ai102demos/01-analyze-images/py-version/image-analysis.py
@@ -1,12 +1,15 @@
 from dotenv import load_dotenv
 import os
-from PIL import Image, ImageDraw
 import sys
-from matplotlib import pyplot as plt
 from azure.core.exceptions import HttpResponseError
 import requests
+from PIL import Image, ImageDraw
+from matplotlib import pyplot as plt
 
-# Import namespaces
+# import namespaces
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
 
 
 def main():
@@ -27,11 +30,14 @@ def main():
             image_data = f.read()
 
         # Authenticate Azure AI Vision client
+        cv_client = ImageAnalysisClient(
+            endpoint=ai_endpoint,
+            credential=AzureKeyCredential(ai_key)
+        )
 
-        
         # Analyze image
         AnalyzeImage(image_file, image_data, cv_client)
-        
+
         # Background removal
         BackgroundForeground(ai_endpoint, ai_key, image_file)
 
@@ -44,23 +50,80 @@ def AnalyzeImage(image_filename, image_data, cv_client):
 
     try:
         # Get result with specified features to be retrieved
-        
-
+        result = cv_client.analyze(
+            image_data=image_data,
+            visual_features=[
+                VisualFeatures.CAPTION,
+                VisualFeatures.DENSE_CAPTIONS,
+                VisualFeatures.TAGS,
+                VisualFeatures.OBJECTS,
+                VisualFeatures.PEOPLE],
+        )
     except HttpResponseError as e:
         print(f"Status code: {e.status_code}")
         print(f"Reason: {e.reason}")
         print(f"Message: {e.error.message}")
 
     # Display analysis results
-    
+    # Get image captions
+    if result.caption is not None:
+        print("\nCaption:")
+        print(" Caption: '{}' (confidence: {:.2f}%)".format(
+            result.caption.text, result.caption.confidence * 100))
+
+    # Get image dense captions
+    if result.dense_captions is not None:
+        print("\nDense Captions:")
+        for caption in result.dense_captions.list:
+            print(" Caption: '{}' (confidence: {:.2f}%)".format(
+                caption.text, caption.confidence * 100))
+
+    # Get image tags
+    if result.tags is not None:
+        print("\nTags:")
+        for tag in result.tags.list:
+            print(" Tag: '{}' (confidence: {:.2f}%)".format(
+                tag.name, tag.confidence * 100))
+
+    # Get objects in the image
+    if result.objects is not None:
+        print("\nObjects in image:")
+
+        # Prepare image for drawing
+        image = Image.open(image_filename)
+        fig = plt.figure(figsize=(image.width/100, image.height/100))
+        plt.axis('off')
+        draw = ImageDraw.Draw(image)
+        color = 'cyan'
+
+        for detected_object in result.objects.list:
+            # Print object name
+            print(" {} (confidence: {:.2f}%)".format(
+                detected_object.tags[0].name, detected_object.tags[0].confidence * 100))
+
+            # Draw object bounding box
+            r = detected_object.bounding_box
+            bounding_box = ((r.x, r.y), (r.x + r.width, r.y + r.height))
+            draw.rectangle(bounding_box, outline=color, width=3)
+            plt.annotate(
+                detected_object.tags[0].name, (r.x, r.y), backgroundcolor=color)
+
+        # Save annotated image
+        plt.imshow(image)
+        plt.tight_layout(pad=0)
+        outputfile = 'objects.jpg'
+        fig.savefig(outputfile)
+        print('  Results saved in', outputfile)
+
+    # Get people in the image
+
 
 def BackgroundForeground(endpoint, key, image_file):
     # Define the API version and mode
     api_version = "2023-02-01-preview"
-    mode="backgroundRemoval" # Can be "foregroundMatting" or "backgroundRemoval"
-    
+    mode = "backgroundRemoval"  # Can be "foregroundMatting" or "backgroundRemoval"
+
     # Remove the background from the image or generate a foreground matte
-    
 
 
 if __name__ == "__main__":
--- ai102demos/ReadMe.md
+++ ai102demos/ReadMe.md
@@ -9,4 +9,9 @@ python -m venv .venv
 .\.venv\Scripts\activate
 
 python.exe -m pip install --upgrade pip
+
+pip install python-dotenv
+pip install Pillow
+pip install matplotlib
+python image-analysis.py images/street.jpg
 ```
