c1f112654ceda749d393228a1894fd8705767361
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-08 16:15:59 +0530

    Update a2-openai-model.py

--- ai102demos/01-aoai/py-version/src/a2-openai-model.py
+++ ai102demos/01-aoai/py-version/src/a2-openai-model.py
@@ -1,7 +1,5 @@
 import os
 from dotenv import load_dotenv
-
-# Add Azure OpenAI package
 from openai import AzureOpenAI
 
 
@@ -9,8 +7,8 @@ def main():
 
     try:
 
-        # Get configuration settings
         load_dotenv()
+
         azure_oai_endpoint = os.getenv("AZURE_OAI_ENDPOINT")
         azure_oai_key = os.getenv("AZURE_OAI_KEY")
         azure_oai_deployment = os.getenv("AZURE_OAI_DEPLOYMENT")
@@ -19,21 +17,15 @@ def main():
         client = AzureOpenAI(
             azure_endpoint=azure_oai_endpoint,
             api_key=azure_oai_key,
-            # Target version of the API, such as 2024-02-15-preview
             api_version="2024-02-15-preview"
         )
 
-        response = client.chat.completions.create(
-            model=azure_oai_deployment,
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": "What is Azure OpenAI?"}
-            ]
-        )
-        generated_text = response.choices[0].message.content
-
-        # Print the response
-        print("Response: " + generated_text + "\n")
+        # Create a system message
+        system_message = """I am a hiking enthusiast named Forest who helps people discover hikes in their area. 
+            If no area is specified, I will default to near Rainier National Park. 
+            I will then provide three suggestions for nearby hikes that vary in length. 
+            I will also share an interesting fact about the local nature on the hikes when making a recommendation.
+            """
 
         while True:
             # Get input text
@@ -47,6 +39,20 @@ def main():
             print("\nSending request for summary to Azure OpenAI endpoint...\n\n")
 
             # Add code to send request...
+            # Send request to Azure OpenAI model
+            response = client.chat.completions.create(
+                model=azure_oai_deployment,
+                temperature=0.7,
+                max_tokens=400,
+                messages=[
+                    {"role": "system", "content": system_message},
+                    {"role": "user", "content": input_text}
+                ]
+            )
+            generated_text = response.choices[0].message.content
+
+            # Print the response
+            print("Response: " + generated_text + "\n")
 
     except Exception as ex:
         print(ex)
