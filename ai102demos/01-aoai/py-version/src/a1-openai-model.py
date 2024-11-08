534eb623a3a595d0e90e277d773f8ea47d4603f3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-11-08 16:10:27 +0530

    Update a1-openai-model.py

--- ai102demos/01-aoai/py-version/src/a1-openai-model.py
+++ ai102demos/01-aoai/py-version/src/a1-openai-model.py
@@ -1,7 +1,5 @@
 import os
 from dotenv import load_dotenv
-
-# Add Azure OpenAI package
 from openai import AzureOpenAI
 
 
@@ -9,19 +7,20 @@ def main():
 
     try:
 
-        # Get configuration settings
         load_dotenv()
+
         azure_oai_endpoint = os.getenv("AZURE_OAI_ENDPOINT")
         azure_oai_key = os.getenv("AZURE_OAI_KEY")
         azure_oai_deployment = os.getenv("AZURE_OAI_DEPLOYMENT")
 
-        # Initialize the Azure OpenAI client...
         client = AzureOpenAI(
             azure_endpoint=azure_oai_endpoint,
             api_key=azure_oai_key,
-            # Target version of the API, such as 2024-02-15-preview
             api_version="2024-02-15-preview"
         )
+
+        print("Azure OpenAI Chat Completions Example\n")
+        print("Starting a chat completion request... Please wait ....\n")
 
         response = client.chat.completions.create(
             model=azure_oai_deployment,
@@ -34,19 +33,6 @@ def main():
 
         # Print the response
         print("Response: " + generated_text + "\n")
-
-        while True:
-            # Get input text
-            input_text = input("Enter the prompt (or type 'quit' to exit): ")
-            if input_text.lower() == "quit":
-                break
-            if len(input_text) == 0:
-                print("Please enter a prompt.")
-                continue
-
-            print("\nSending request for summary to Azure OpenAI endpoint...\n\n")
-
-            # Add code to send request...
 
     except Exception as ex:
         print(ex)
